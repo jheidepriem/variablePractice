22f38df526989673260e25c040083c50c6b5734d
Josephine Heidepriem <jheidepriem@gmail.com>
2022-07-08 13:38:07 -0500

    Add console log for variables practice

--- variables.js
+++ variables.js
@@ -4,3 +4,9 @@ var status = "online"
 var age = 37;
 var sentence = `${age} how old I am`;
 console.log(sentence);
+var email = "josephineh@gmail.com"
+var age = "38"
+var status = "offline"
+console.log(email)
+console.log(age)
+console.log(status)
